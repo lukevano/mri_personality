a2b6d68b8791e072af19f7f074a2a9fb1ae03f0d
Christopher Holmes <c.holmes.silva@gmail.com>
2021-09-11 17:50:04 +0200

    Front end with sidebar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from mri_personality.data import clean_data
+from mri_personality.data import clean_data, get_data
 from sklearn.preprocessing import StandardScaler
 import streamlit as st
 import numpy as np
@@ -16,6 +16,27 @@ st.set_page_config(
 '''
 # Personality Cortex
 '''
+st.sidebar.title('LW Batch 602 London')
+st.sidebar.subheader('Christopher Holmes, Francisco Costa, Luke Vano')
+st.sidebar.markdown('Project explores different models to predict big 5 personality types based on brain thickness')
+st.markdown('##')
+st.markdown('##')
+st.markdown('##')
+
+
+wiki_link='[Wikipedia](https://en.wikipedia.org/wiki/Big_Five_personality_traits)'
+st.sidebar.markdown('What are the Big 5 personality traits?')
+st.sidebar.markdown(wiki_link, unsafe_allow_html=True)
+data_link = '[Nilab Amsterdam](https://nilab-uva.github.io/AOMIC.github.io/)'
+st.sidebar.markdown('Data Source:')
+st.sidebar.markdown(data_link, unsafe_allow_html=True)
+source_link = '[Oxford University Press](https://pubmed.ncbi.nlm.nih.gov/28122961/)'
+st.sidebar.markdown('Neuroanatomy basis for Big 5 personalities:')
+st.sidebar.markdown(source_link, unsafe_allow_html=True)
+python_link = '[NeuroImg - Python](https://nipy.org/nibabel/reference/nibabel.freesurfer.html#nibabel.freesurfer.io.read_geometry/)'
+st.sidebar.markdown('Library for neuro imaging files:')
+st.sidebar.markdown(python_link, unsafe_allow_html=True)
+
 
 st.text('Curious about what type of personality do you have?')
 
@@ -26,20 +47,13 @@ st.markdown('No MRI? No problem. First insert your parameters.')
 
 sex= st.selectbox('Insert your sex', ('Male', 'Female'))
 
-#age= st.text_input('Insert your age')
-
-#bmi= st.text_input('Insert your BMI')
-
-#hand=st.selectbox('What hand do you use?', ('right', 'left','ambidextrous'))
-
-#education=st.selectbox('Insert your level of education', ('applied', 'academic','medium','high','low'))
 
 uploaded_csv=st.file_uploader("Upload the CSV file", type="xlsx")
 
 
 st.markdown('Do you have an MRI? Upload the **NII image** and see the prediction.')
 
-uploaded_image=st.file_uploader("Upload an MRI", type="NII")
+uploaded_image=st.file_uploader("Upload an MRI", type="JPG")
 
 
 
@@ -87,15 +101,15 @@ if uploaded_csv!=None:
         
         # This is the proba that they fall in each category
         m_knn_proba = pd.DataFrame({'Leader': m_knn_0_results,
-                            'Task orientatied': m_knn_1_results,
+                            'Task oriented': m_knn_1_results,
                             'Maverick': m_knn_2_results,
                             'Anxious': m_knn_3_results,
                             'Workaholic': m_knn_4_results})
-        results={'Leader': '....','Task': '....'}
+        results={'Leader': '....','Task oriented': '....','Maverick': '....','Anxious':'...','Workaholic':'...'}
         # This is the category they are highest in
-        'Results per category:',m_knn_proba
+        
         fig = go.Figure(data=go.Scatterpolar(
-        r=m_knn_proba.values,
+        r=m_knn_proba.values[0],
         theta=m_knn_proba.columns.tolist(),
         fill='toself'))
         fig.update_layout(
@@ -105,9 +119,8 @@ if uploaded_csv!=None:
         showlegend=False)
 
         st.plotly_chart(fig)
-        st.text(m_knn_proba.values)
-        st.text(m_knn_proba.columns.tolist())
-        st.text(m_knn_proba.idxmax(axis = 1, skipna = True))
+        
+        st.text(results[m_knn_proba.idxmax(axis = 1, skipna = True).values[0]])
 
         
         
@@ -123,26 +136,131 @@ if uploaded_csv!=None:
     'Right-non-WM-hypointensities'],axis=1,inplace=True)
         uploaded_df=pd.DataFrame(scaler.fit_transform(uploaded_df),columns=uploaded_df.columns)
         uploaded_df=sm.add_constant(uploaded_df)
-        f_knn_0_results=f_knn_0.predict(uploaded_df.iloc[3,:].values.T.reshape(1,-1))
-        f_knn_1_results=f_knn_1.predict(uploaded_df.iloc[3,:].values.T.reshape(1,-1))
-        f_knn_2_results=f_knn_2.predict(uploaded_df.iloc[3,:].values.T.reshape(1,-1))
-        f_knn_3_results=f_knn_3.predict(uploaded_df.iloc[3,:].values.T.reshape(1,-1))
-        f_knn_4_results=f_knn_4.predict(uploaded_df.iloc[3,:].values.T.reshape(1,-1))
-
+        f_knn_0_results=f_knn_0.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_1_results=f_knn_1.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_2_results=f_knn_2.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_3_results=f_knn_3.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_4_results=f_knn_4.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        
         # This is the proba that they fall in each category
         f_knn_proba = pd.DataFrame({'Leader': f_knn_0_results,
-                            'Task orientatied': f_knn_1_results,
+                            'Task oriented': f_knn_1_results,
                             'Maverick': f_knn_2_results,
                             'Anxious': f_knn_3_results,
                             'Workaholic': f_knn_4_results})
+        results={'Leader': '....','Task oriented': '....','Maverick': '....','Anxious':'...','Workaholic':'...'}
+        # This is the proba that they fall in each category
+        fig = go.Figure(data=go.Scatterpolar(
+        r=f_knn_proba.values[0],
+        theta=f_knn_proba.columns.tolist(),
+        fill='toself'))
+        fig.update_layout(
+         polar=dict(
+        radialaxis=dict(
+        visible=True),),
+        showlegend=False)
+
+        st.plotly_chart(fig)
         
-        category_dict={'Leader': '....','Task orientatied': '....','Maverick': '....','Anxious': '....','Workaholic': '....'}
-        # This is the category they are highest in
-        'Results per category:',m_knn_proba 
-        st.text(m_knn_proba.idxmax(axis = 1, skipna = True))
+        st.text(results[f_knn_proba.idxmax(axis = 1, skipna = True).values[0]])
     
-else:
-    st.text('Here we should see the CNN prediction')
+elif uploaded_image!=None:
+    f_knn_0=pickle.load(open('mri_personality/f_knn_0.pickle', 'rb'))
+    f_knn_1=pickle.load(open('mri_personality/f_knn_1.pickle', 'rb'))
+    f_knn_2=pickle.load(open('mri_personality/f_knn_2.pickle', 'rb'))
+    f_knn_3=pickle.load(open('mri_personality/f_knn_3.pickle', 'rb'))
+    f_knn_4=pickle.load(open('mri_personality/f_knn_4.pickle', 'rb'))
+    m_knn_0=pickle.load(open('mri_personality/m_knn_0.pickle', 'rb'))
+    m_knn_1=pickle.load(open('mri_personality/m_knn_1.pickle', 'rb'))
+    m_knn_2=pickle.load(open('mri_personality/m_knn_2.pickle', 'rb'))
+    m_knn_3=pickle.load(open('mri_personality/m_knn_3.pickle', 'rb'))
+    m_knn_4=pickle.load(open('mri_personality/m_knn_4.pickle', 'rb'))
+    uploaded_df=pd.read_excel('mri_personality/data/master_combined.xlsx')
+    
+    if sex == 'Male':
+        #uploaded_df=uploaded_df[uploaded_df['sex']=='']
+        uploaded_df.drop(['participant_id','age','sex','BMI','handedness','education_category','NEO_N','NEO_E','NEO_O','NEO_A',
+                          'NEO_C','eTIV.1', 'EstimatedTotalIntraCranialVol', 'BrainSegVolNotVent.2',
+                         'BrainSegVolNotVent.1', 'BrainSegVolNotVentSurf', 'SupraTentorialVolNotVentVox',
+    'lhCerebralWhiteMatterVol', 'rhCerebralWhiteMatterVol', 'BrainSegVolNotVent.2', 
+    'BrainSegVol', 'SupraTentorialVol', 'SupraTentorialVolNotVent',
+    'BrainSegVol-to-eTIV', 'MaskVol', 'rhCortexVol', 'lhCortexVol', 'Left-WM-hypointensities',
+    'Right-WM-hypointensities', 'non-WM-hypointensities', 'Left-non-WM-hypointensities',
+    'Right-non-WM-hypointensities'],axis=1,inplace=True)
+        uploaded_df=pd.DataFrame(scaler.fit_transform(uploaded_df),columns=uploaded_df.columns)
+        uploaded_df=sm.add_constant(uploaded_df)
+        
+        m_knn_0_results=m_knn_0.predict(uploaded_df.iloc[4,:].values.T.reshape(1,-1))
+        m_knn_1_results=m_knn_1.predict(uploaded_df.iloc[4,:].values.T.reshape(1,-1))
+        m_knn_2_results=m_knn_2.predict(uploaded_df.iloc[4,:].values.T.reshape(1,-1))
+        m_knn_3_results=m_knn_3.predict(uploaded_df.iloc[4,:].values.T.reshape(1,-1))
+        m_knn_4_results=m_knn_4.predict(uploaded_df.iloc[4,:].values.T.reshape(1,-1))
+        
+        # This is the proba that they fall in each category
+        m_knn_proba = pd.DataFrame({'Leader': m_knn_0_results,
+                            'Task oriented': m_knn_1_results,
+                            'Maverick': m_knn_2_results,
+                            'Anxious': m_knn_3_results,
+                            'Workaholic': m_knn_4_results})
+        results={'Leader': '....','Task oriented': '....','Maverick': '....','Anxious':'...','Workaholic':'...'}
+        # This is the category they are highest in
+        
+        fig = go.Figure(data=go.Scatterpolar(
+        r=m_knn_proba.values[0],
+        theta=m_knn_proba.columns.tolist(),
+        fill='toself'))
+        fig.update_layout(
+         polar=dict(
+        radialaxis=dict(
+        visible=True),),
+        showlegend=False)
+
+        st.plotly_chart(fig)
+        
+        st.text(results[m_knn_proba.idxmax(axis = 1, skipna = True).values[0]])
+
+        
+        
+    if sex == 'Female':
+        #uploaded_df=uploaded_df[uploaded_df['sex']=='']
+        uploaded_df.drop(['participant_id','age','sex','BMI','handedness','education_category','NEO_N','NEO_E','NEO_O','NEO_A',
+                          'NEO_C','eTIV.1', 'EstimatedTotalIntraCranialVol', 'BrainSegVolNotVent.2',
+                         'BrainSegVolNotVent.1', 'BrainSegVolNotVentSurf', 'SupraTentorialVolNotVentVox',
+    'lhCerebralWhiteMatterVol', 'rhCerebralWhiteMatterVol', 'BrainSegVolNotVent.2', 
+    'BrainSegVol', 'SupraTentorialVol', 'SupraTentorialVolNotVent',
+    'BrainSegVol-to-eTIV', 'MaskVol', 'rhCortexVol', 'lhCortexVol', 'Left-WM-hypointensities',
+    'Right-WM-hypointensities', 'non-WM-hypointensities', 'Left-non-WM-hypointensities',
+    'Right-non-WM-hypointensities'],axis=1,inplace=True)
+        uploaded_df=pd.DataFrame(scaler.fit_transform(uploaded_df),columns=uploaded_df.columns)
+        uploaded_df=sm.add_constant(uploaded_df)
+        f_knn_0_results=f_knn_0.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_1_results=f_knn_1.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_2_results=f_knn_2.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_3_results=f_knn_3.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        f_knn_4_results=f_knn_4.predict(uploaded_df.iloc[0,:].values.T.reshape(1,-1))
+        
+        # This is the proba that they fall in each category
+        f_knn_proba = pd.DataFrame({'Leader': f_knn_0_results,
+                            'Task oriented': f_knn_1_results,
+                            'Maverick': f_knn_2_results,
+                            'Anxious': f_knn_3_results,
+                            'Workaholic': f_knn_4_results})
+        results={'Leader': '....','Task oriented': '....','Maverick': '....','Anxious':'...','Workaholic':'...'}
+        # This is the proba that they fall in each category
+        fig = go.Figure(data=go.Scatterpolar(
+        r=f_knn_proba.values[0],
+        theta=f_knn_proba.columns.tolist(),
+        fill='toself'))
+        fig.update_layout(
+         polar=dict(
+        radialaxis=dict(
+        visible=True),),
+        showlegend=False)
+
+        st.plotly_chart(fig)
+        
+        st.text(results[f_knn_proba.idxmax(axis = 1, skipna = True).values[0]])
+    
 
 
 
